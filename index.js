6f98f8fd16b639e5ae0445d7ae8d229ee7d886ab
Jacob Corum <cor15031@byui.edu>
2020-06-25 23:07:05 -0600

    promises created for writefile and generateTitle

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const { rejects } = require("assert");
+const { resolve } = require("path");
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile("./dist/index.html", fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: "File created!!"
+            })
+        })
+    })
+}
+
+const generateTitle = readmeData => readmeData.title
 
 const promptUser = () => {
     inquirer.prompt([
         {
-            type: "input", 
-            name: "name", 
-            message: "Project name?"
+            type: "input",
+            name: "title",
+            message: "Project title?"
         }
     ])
 }
+
+promptUser()
+    .then(readmeData => generateTitle(readmeData))
+    .then(pageMd => writeFile(pageMd))
