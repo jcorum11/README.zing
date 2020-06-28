1979598ca144c32295cbd4df225f14a142c9192f
Jacob Corum <cor15031@byui.edu>
2020-06-27 18:06:19 -0600

    bug fix

--- Develop/utils/licenseSelector.js
+++ Develop/utils/licenseSelector.js
@@ -8,49 +8,49 @@ const licenseSelector = readmeData => {
             }
             return license;
         case `GNU AGPLv3`:
-            let license = {
+            license = {
                 "name": "GNU AGPLv3",
                 "link": `[GNU AGPLv3](https://choosealicense.com/licenses/agpl-3.0/)`,
                 "description": "Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available."
             }
             return license;
         case `GNU GPLv3`:
-            let license = {
+            license = {
                 "name": "GNU GPLv3",
                 "link": `[GNU GPLv3](https://choosealicense.com/licenses/gpl-3.0/)`,
                 "description": "Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights."
             }
             return license;
         case `GNU LGPLv3`:
-            let license = {
+            license = {
                 "name": "GNU LGPLv3",
                 "link": `[GNU LGPLv3](https://choosealicense.com/licenses/lgpl-3.0/)`,
                 "description": "Permissions of this copyleft license are conditioned on making available complete source code of licensed works and modifications under the same license or the GNU GPLv3. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work through interfaces provided by the licensed work may be distributed under different terms and without source code for the larger work."
             }
             return license;
         case `Mozilla Public License 2.0`:
-            let license = {
+            license = {
                 "name": "Mozilla Public License 2.0",
                 "link": `[Mozilla Public License 2.0](https://choosealicense.com/licenses/mpl-2.0/)`,
                 "description": "Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work."
             }
             return license;
         case `Apache License 2.0`:
-            let license = {
+            license = {
                 "name": "Apache License 2.0",
                 "link": `[Apache License 2.0](https://choosealicense.com/licenses/apache-2.0/)`,
                 "description": "A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code."
             }
             return license;
         case `Boost Software License 1.0`:
-            let license = {
+            license = {
                 "name": "Boost Software License 1.0",
                 "link": `[Boost Software License 1.0](https://choosealicense.com/licenses/bsl-1.0/)`,
                 "description": "A simple permissive license only requiring preservation of copyright and license notices for source (and not binary) distribution. Licensed works, modifications, and larger works may be distributed under different terms and without source code."
             }
             return license;
         case `The Unlicense`:
-            let license = {
+            license = {
                 "name": "The Unlicense",
                 "link": `[The Unlicense](https://choosealicense.com/licenses/unlicense/)`,
                 "description": "A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code."
