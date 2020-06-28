2ceeaa4551315f852ae3bcac2919ae2f274bf8a5
Jacob Corum <cor15031@byui.edu>
2020-06-27 18:03:20 -0600

    bug fix

--- Develop/utils/licenseSelector.js
+++ Develop/utils/licenseSelector.js
@@ -2,8 +2,8 @@ const licenseSelector = readmeData => {
     switch (readmeData) {
         case `MIT License`:
             const license = {
-                "name": "MIT"
-                "link": `[MIT License](https://choosealicense.com/licenses/mit/)`,,
+                "name": "MIT",
+                "link": `[MIT License](https://choosealicense.com/licenses/mit/)`,
                 "description": "A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code."
             }
             return license;
